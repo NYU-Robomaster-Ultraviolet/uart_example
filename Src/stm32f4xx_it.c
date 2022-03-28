48fb2dcf18fcf56473ff6a73de20d5a98b73a59e
Jerry Wu <jerrywuzijie@gmail.com>
2022-03-28 17:08:44 -0400

    feat: re-compile project using vscode

--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -197,21 +197,20 @@ void SysTick_Handler(void)
 /* please refer to the startup file (startup_stm32f4xx.s).                    */
 /******************************************************************************/
 
-/**
-  * @brief This function handles USART1 global interrupt.
-  */
-//void USART1_IRQHandler(void)
-//{
-//  /* USER CODE BEGIN USART1_IRQn 0 */
+// /**
+//   * @brief This function handles USART1 global interrupt.
+//   */
+// void USART1_IRQHandler(void)
+// {
+//   /* USER CODE BEGIN USART1_IRQn 0 */
 
-//  /* USER CODE END USART1_IRQn 0 */
-//  HAL_UART_IRQHandler(&huart1);
-//  /* USER CODE BEGIN USART1_IRQn 1 */
+//   /* USER CODE END USART1_IRQn 0 */
+//   HAL_UART_IRQHandler(&huart1);
+//   /* USER CODE BEGIN USART1_IRQn 1 */
 
-//  /* USER CODE END USART1_IRQn 1 */
-//}
+//   /* USER CODE END USART1_IRQn 1 */
+// }
 
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
